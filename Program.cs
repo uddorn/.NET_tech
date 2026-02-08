d5d230fe769df0b3e3f16d434b84ba15e8919345
Tkachenko Dmytro <tkachenkoff91@gmail.com>
2026-02-08 23:40:36 +0200

    Лабораторна робота №1_Вар№6

---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1IsHere
+{
+    public abstract class Organization
+    {
+        public string Name { get; set; }
+
+        public Organization(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Організація: {Name}");
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Organization other)
+                return this.Name == other.Name;
+            return false;
+        }
+
+        public override int GetHashCode() => Name.GetHashCode();
+    }
+
+    public class InsuranceCompany : Organization
+    {
+        public string AgentName { get; set; }
+        public int PolicyCount { get; set; }
+        public double TotalAmount { get; set; }
+
+        public InsuranceCompany(string name, string agent, int policies, double amount)
+            : base(name)
+        {
+            AgentName = agent;
+            PolicyCount = policies;
+            TotalAmount = amount;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Страхова компанія '{Name}': Агент {AgentName}, " +
+                              $"Кількість полісів: {PolicyCount}, Сума: {TotalAmount} грн.");
+        }
+    }
+
+    public class ConstructionCompany : Organization
+    {
+        public string ForemanName { get; set; }
+        public double Estimate { get; set; }
+        public double Area { get; set; }
+
+        public ConstructionCompany(string name, string foreman, double estimate, double area)
+            : base(name)
+        {
+            ForemanName = foreman;
+            Estimate = estimate;
+            Area = area;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Будівельна компанія '{Name}': Прораб {ForemanName}, " +
+                              $"Смета: {Estimate} грн, Площа: {Area} кв.м.");
+        }
+    }
+
+    public class MetroBuild : Organization
+    {
+        public string RouteName { get; set; }
+        public int PassengerCount { get; set; }
+        public double Distance { get; set; }
+
+        public MetroBuild(string name, string route, int passengers, double distance)
+            : base(name)
+        {
+            RouteName = route;
+            PassengerCount = passengers;
+            Distance = distance;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Метробуд '{Name}': Маршрут '{RouteName}', " +
+                              $"Пасажирів: {PassengerCount}, Відстань: {Distance} км.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Organization> organizations = new List<Organization>
+            {
+                new InsuranceCompany("ТАС", "Рубан", 150, 500000),
+                new ConstructionCompany("КАН Девелопмент", "Муляр", 2000000, 1200.5),
+                new MetroBuild("Київський Метрополітен", "Синя гілка", 45000, 22.7)
+            };
+
+            Console.WriteLine("Пізнє зв'язування");
+            foreach (var org in organizations)
+            {
+                org.DisplayInfo();
+            }
+
+            Console.WriteLine("\nРаннє зв'язування");
+            InsuranceCompany specificCompany = new InsuranceCompany("Тест", "Тестовий", 1, 100);
+            specificCompany.DisplayInfo();
+
+            Console.WriteLine("\nПеревірка методу Equals");
+            Organization org1 = new MetroBuild("Метро", "А", 10, 5);
+            Organization org2 = new MetroBuild("Метро", "Б", 20, 10);
+            Console.WriteLine($"Чи однакові назви організацій (Equals): {org1.Equals(org2)}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("lab1IsHere")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab1IsHere")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("36bb343d-00d0-480e-9705-6a87a382b922")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
