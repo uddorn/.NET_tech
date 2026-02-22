876bd7707b3c645ab8e102810d601ec02e6635d6
Tkachenko Dmytro <tkachenkoff91@gmail.com>
2026-02-22 22:14:18 +0200

    Лабораторна робота №2_Вар№4

---
+++ lab2IsHere/lab2IsHere/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class ArrayHelper
+{
+    public void PrintArray(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine("Масив порожній.");
+            return;
+        }
+        Console.WriteLine(string.Join(", ", array));
+    }
+
+    public int RemoveAndReturn(ref int[] array, int index)
+    {
+        if (array == null || index < 0 || index >= array.Length)
+        {
+            throw new IndexOutOfRangeException("Некоректний індекс.");
+        }
+
+        int removedElement = array[index];
+        int[] newArray = new int[array.Length - 1];
+
+        for (int i = 0, j = 0; i < array.Length; i++)
+        {
+            if (i == index) continue;
+            newArray[j++] = array[i];
+        }
+
+        array = newArray;
+        return removedElement;
+    }
+}
---
+++ lab2IsHere/lab2_1IsHere/lab2_1IsHere/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+
+namespace lab2_1IsHere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Assembly a = Assembly.LoadFrom("lab2IsHere.dll");
+
+                Type t = a.GetType("ArrayHelper");
+                object o = a.CreateInstance("ArrayHelper");
+
+                int[] myArr = { 10, 20, 30, 40, 50 };
+
+                Console.WriteLine("Початковий масив:");
+                MethodInfo printMethod = t.GetMethod("PrintArray");
+                object[] printArgs = new object[] { myArr };
+                printMethod.Invoke(o, printArgs);
+
+                Console.WriteLine("\nВидаляємо елемент за індексом 2 (значення 30)...");
+                MethodInfo removeMethod = t.GetMethod("RemoveAndReturn");
+                object[] removeArgs = new object[] { myArr, 2 };
+                object removedValue = removeMethod.Invoke(o, removeArgs);
+
+                myArr = (int[])removeArgs[0];
+                Console.WriteLine($"Видалений елемент: {removedValue}");
+
+                Console.WriteLine("\nОновлений масив:");
+                printArgs[0] = myArr;
+                printMethod.Invoke(o, printArgs);
+
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка: {ex.Message}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
