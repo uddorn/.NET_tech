1fcfd9c1df0c7106a962af53dcbea96e65eded3b
Tkachenko Dmytro <tkachenkoff91@gmail.com>
2026-04-05 21:37:07 +0300

    Лабораторна робота№7_Вар№7

---
+++ lab7IsHere/lab7IsHere/MainForm.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainForm.aspx.cs" Inherits="lab7IsHere.MainForm" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Лабораторна робота 7</title>
+    <style>
+        body { font-family: Arial, sans-serif; padding: 20px; }
+        .result-box { margin-top: 20px; padding: 15px; border: 1px solid #ccc; background-color: #f9f9f9; }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Анкета</h2>
+            
+            Прізвище: <br />
+            <asp:TextBox ID="TextBox1" runat="server" Width="200px"></asp:TextBox> <br /><br />
+            
+            Ім&#39;я: <br />
+            <asp:TextBox ID="TextBox2" runat="server" Width="200px"></asp:TextBox> <br /><br />
+            
+            По-батькові: <br />
+            <asp:TextBox ID="TextBox3" runat="server" Width="200px"></asp:TextBox> <br /><br />
+            
+            Email адреса:<br />
+            <asp:TextBox ID="TextBox4" runat="server" Width="200px"></asp:TextBox> <br /><br />
+            
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Перевірити дані" Height="30px" /> <br />
+            
+            <div class="result-box">
+                <strong>Результат:</strong><br /><br />
+                <asp:Label ID="Label1" runat="server" ForeColor="DarkBlue"></asp:Label>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ lab7IsHere/lab7IsHere/MainForm.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions; 
+using System.Collections.Generic;     
+
+namespace lab7IsHere
+{
+    public partial class MainForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string surname = TextBox1.Text.Trim();
+            string name = TextBox2.Text.Trim();
+            string patronymic = TextBox3.Text.Trim();
+            string email = TextBox4.Text.Trim();
+
+            List<string> emptyFields = new List<string>();
+
+            if (string.IsNullOrEmpty(surname)) emptyFields.Add("Прізвище");
+            if (string.IsNullOrEmpty(name)) emptyFields.Add("Ім'я");
+            if (string.IsNullOrEmpty(patronymic)) emptyFields.Add("По батькові");
+            if (string.IsNullOrEmpty(email)) emptyFields.Add("Email");
+
+            if (emptyFields.Count > 0)
+            {
+                Label1.ForeColor = System.Drawing.Color.Red;
+                Label1.Text = "Помилка: Будь ласка, заповніть наступні поля: " + string.Join(", ", emptyFields) + "!";
+                return;
+            }
+
+            string[] words = { surname, name, patronymic };
+
+            string thirdWord = words[2];
+
+            int surnameLength = surname.Count(char.IsLetter);
+
+            string shortestWord = words.OrderBy(w => w.Length).First();
+
+            bool isEmailValid = false;
+
+            string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+
+            if (!string.IsNullOrEmpty(email) && Regex.IsMatch(email, emailPattern))
+            {
+                isEmailValid = true;
+            }
+
+            Label1.ForeColor = System.Drawing.Color.DarkGreen;
+            Label1.Text = $"<strong>Третє слово:</strong> {thirdWord} <br/>" +
+                          $"<strong>Кількість літер у прізвищі:</strong> {surnameLength} <br/>" +
+                          $"<strong>Найкоротше слово:</strong> {shortestWord} <br/>" +
+                          $"<strong>Чи є введений текст email адресою?</strong> {(isEmailValid ? "Так, це email" : "Ні, формат некоректний")}";
+        }
+    }
+}
